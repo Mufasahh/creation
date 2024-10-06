1f2f20c922e68f9518248668eedc0a58d4f2a3ca
Mufasahh <mufasahh1@gmail.com>
2024-10-06 01:18:20 +0100

    I added a function to allow the user of the function to create a new animal

--- constructor.rs
+++ constructor.rs
@@ -10,3 +10,26 @@ struct Animal {
     age: u32,
     territory: String,
 }
+
+impl Animal {
+    fn create_animal(
+        animal_name: &str,
+        age: u32,
+        territory: &str,
+    ) -> Self {
+        Self {
+            animal_name: String::from(animal_name),
+            age: u32,
+            territory: String::from(territory),
+        }
+    }
+
+    fn print_animal(&self) {
+        let Animal {
+            animal_name,
+            age,
+            territory,
+        } = self;
+        println!("I created a new animal called {animal_name}, he is {age} years old, and he live in a {territory}");
+    }
+}
